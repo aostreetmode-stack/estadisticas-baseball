e1c6929b733b2f8ba05cf6efb65cff67228824dd
aostreetmode-stack <aostreetmode@gmail.com>
2026-03-24 20:01:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', e => {
+self.addEventListener('install', event => {
   self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log("Service Worker activo");
+});
+
+self.addEventListener('fetch', event => {
+  // No hacemos cache aún para no romper nada
 });
