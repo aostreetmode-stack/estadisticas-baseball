690bd29626e3ba6cb6622d439b5c7fc9dd11564a
aostreetmode-stack <aostreetmode@gmail.com>
2026-03-24 20:01:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-self.addEventListener("install", e=>{
+self.addEventListener('install', e => {
   self.skipWaiting();
-});
-
-self.addEventListener("fetch", e=>{
-  e.respondWith(fetch(e.request));
 });
